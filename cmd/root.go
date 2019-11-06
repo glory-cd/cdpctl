8d2a70a14355a3fb03b821cf9a5783072e287efe
zhangxiaohui <zhangxiaohui@auto-fis.com>
2019-11-06 17:12:09 +0800

    delete conn server in the file and read config info from config file

--- cmd/root.go
+++ cmd/root.go
@@ -16,85 +16,98 @@ limitations under the License.
 package cmd
 
 import (
-  "fmt"
-  "github.com/glory-cd/server/client"
-  "github.com/mitchellh/go-homedir"
-  "github.com/spf13/cobra"
-  "github.com/spf13/viper"
-  "os"
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
+	"path"
+	"path/filepath"
 )
 
-
-var cfgFile string
+var (
+	version bool
+)
 
 // rootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
-  Use:   "cdpctl",
-  Short: "A Continuous Deployment Process Tool",
-  Long:  ``,
-  // Uncomment the following line if your bare application
-  // has an action associated with it:
-  //	Run: func(cmd *cobra.Command, args []string) { },
-  PersistentPreRun: func(cmd *cobra.Command, args []string) {
-    var err error
-    cdpattr := client.CDPCClientAttr{CertFile: "cert/server.crt", Address: "localhost:50051"}
-    //cdpattr := client.CDPCClientAttr{CertFile: "cert_75/server.crt", Address: "192.168.1.75:30051"}
-    MyConn, err = client.NewClient(cdpattr)
-    if err != nil {
-      cmd.PrintErrf("Conn Server failed. [%s]", err)
-      return
-    }
-  },
+	Use:   "cdpctl",
+	Short: "A Continuous Deployment Process Tool",
+	Long:  ``,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		if version {
+			fmt.Println(versionString)
+		}
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-  if err := RootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-  cobra.OnInitialize(initConfig)
-
-  // Here you will define your flags and configuration settings.
-  // Cobra supports persistent flags, which, if defined here,
-  // will be global for your application.
-
-  //rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cdpctl.yaml)")
-
-
-  // Cobra also supports local flags, which will only run
-  // when this action is called directly.
-  //rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	cobra.OnInitialize(initConfig)
+	RootCmd.Flags().BoolVarP(&version, "version", "", false, "the application version")
 }
-
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-  if cfgFile != "" {
-    // Use config file from the flag.
-    viper.SetConfigFile(cfgFile)
-  } else {
-    // Find home directory.
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
+	// Find home directory.
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-    // Search config in home directory with name ".cdpctl" (without extension).
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".cdpctl")
-  }
+	// Search config in home directory with name ".cdpctl" (without extension).
+	viper.AddConfigPath(home)
+	viper.SetConfigName(".cdpctl")
+	viper.SetConfigType("toml")
 
-  viper.AutomaticEnv() // read in environment variables that match
+	var cfgFile = path.Join(home, ".cdpctl.toml")
 
-  // If a config file is found, read it in.
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err != nil {
+		// if config file not exist,then create config file,and set default value.
+		if viper.ConfigFileUsed() == "" {
+			viper.Set(ServerHostUrlKey, ServerHostUrlKeyDefault)
+			viper.Set(ServerCertFileKey, ServerCertFileDefault)
+			err := viper.WriteConfigAs(cfgFile)
+			if err != nil {
+				RootCmd.PrintErrf("set default config failed. %s\n", err)
+				os.Exit(1)
+			}
+		} else {
+			RootCmd.PrintErrf("%s %s \n", cfgFile, err)
+			os.Exit(1)
+		}
+
+	}
+	hostUrl = viper.GetString(ServerHostUrlKey)
+	givenCertFile := viper.GetString(ServerCertFileKey)
+
+	if givenCertFile == "" {
+		RootCmd.PrintErrf("key certFile is empty.\n")
+		os.Exit(1)
+	}
+
+	if hostUrl == "" {
+		RootCmd.PrintErrf("key hostUrl is empty.\n")
+		os.Exit(1)
+	}
+
+	if ! filepath.IsAbs(givenCertFile) {
+		certFile = path.Join(home, givenCertFile)
+	} else {
+		certFile = givenCertFile
+	}
+
 }
-
